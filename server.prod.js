6de261c8a176be573362e4f7af6c6aae462a1605
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2018-01-10 07:32:50 +0100

    - set the `useMongoClient` option on connect()
    - add e2e testing

--- server.prod.js
+++ server.prod.js
@@ -30,7 +30,7 @@ dotenv.config();
 // database config
 const configDB = require('./server/config/database');
 
-mongoose.connect(configDB.url, option);
+mongoose.createConnection(configDB.url, option);
 const compiler = webpack(prodConfig);
 const port = parseInt(process.env.PORT, 10) || 8000;
 
